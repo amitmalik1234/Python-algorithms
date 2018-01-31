c1bf98ffaa9fd049fa88bfbaab71e97d370684d3
Amit Malik <amitmalik.mca@gmail.com>
2018-01-31 15:20:08 -0800

    test2

--- hello.py
+++ hello.py
@@ -1,1 +1,26 @@
 print('Testing:GitHub with Visual Studio Code')
+
+
+
+print("Hello")
+
+a = "Hi"
+b="a"
+
+val = ord(b)
+print(val)
+
+
+def GetAsciiVal():
+    str_Len = len(a)
+    chars = list(a)
+
+    asciiVal =0
+    for i in range(str_Len):
+        asciiVal = asciiVal + ord(chars[i])
+    return asciiVal
+ 
+asciiVal = GetAsciiVal()
+
+
+print("Convert Hi -- > " + str(asciiVal))
